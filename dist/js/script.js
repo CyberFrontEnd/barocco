ff4760ddb3012056506cdb2548f38daeb2b9a38a
CyberFrontEnd <avoelike@gmail.com>
2024-10-25 23:36:10 +0400

    25,10,2024

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,13 @@
 $(document).ready(function () {
+	$('[data-fancybox]').fancybox({
+		autoFocus: false,
+	});
+});
+
+$(document).ready(function () {
   new WOW().init();
 
 	$(".orderMenu").metisMenu();
-
 
   $(window).scroll(function(){
   		if ($(window).scrollTop() > 50) {
@@ -56,4 +61,117 @@ $(document).ready(function () {
 			}
 		});
 	});
+});
+
+
+$(document).ready(function () {
+	let maxBonus = 0;
+
+	$('#bonusLink').click(function(e) {
+		e.preventDefault();
+
+		maxBonus = parseInt($(this).text(), 10);
+
+		const inputVal = $('#bonusInput').val();
+
+		if (inputVal === "") {
+			$('#bonusInput').val(maxBonus);
+		}
+
+		$('#bonusInput').attr('maxlength', maxBonus.toString().length);
+	});
+
+
+	$('#bonusInput').on('input', function() {
+
+		this.value = this.value.replace(/[^0-9]/g, '');
+
+
+		const currentValue = parseInt(this.value, 10);
+		if (currentValue > maxBonus) {
+			this.value = maxBonus;
+		}
+	});
+});
+
+// Введите код из SMS
+$(document).ready(function() {
+	$('.code-input').first().addClass('active').focus();
+
+
+	$('.code-input').on('keydown', function(e) {
+		if (!$(this).hasClass('active')) {
+			e.preventDefault();
+			return;
+		}
+
+
+		const key = e.which || e.keyCode;
+		if (key < 48 || key > 57) {
+			if (key !== 8 && key !== 9 && (key < 37 || key > 40)) {
+				e.preventDefault();
+			}
+		}
+	});
+
+	$('.code-input').on('input', function() {
+		const currentIndex = $('.code-input').index(this);
+		const currentValue = $(this).val();
+
+		if (currentValue !== '') {
+
+			const allFilled = $('.code-input').slice(0, currentIndex).toArray().every(input => $(input).val() !== '');
+
+			if (allFilled && currentIndex < 5) {
+
+				$('.code-input').eq(currentIndex + 1).addClass('active').focus();
+			}
+		}
+
+
+		if (currentValue === '') {
+			$(this).removeClass('active');
+		}
+	});
+
+	$('.code-input').on('focus', function() {
+		$('.code-input').removeClass('active');
+		$(this).addClass('active');
+	});
+
+	$('.code-input').on('keydown', function(e) {
+		if (e.key === 'Backspace' && $(this).val() === '') {
+			const currentIndex = $('.code-input').index(this);
+			if (currentIndex > 0) {
+				$('.code-input').eq(currentIndex - 1).addClass('active').focus();
+			}
+		}
+	});
+});
+
+
+// Time
+$(document).ready(function() {
+	let timeLeft = 120;
+
+	function updateTimer() {
+		if (timeLeft <= 0) {
+			$('#countdown').text("00:00");
+			clearInterval(timerInterval);
+		} else {
+
+			let minutes = Math.floor(timeLeft / 60);
+			let seconds = timeLeft % 60;
+
+
+			minutes = minutes < 10 ? '0' + minutes : minutes;
+			seconds = seconds < 10 ? '0' + seconds : seconds;
+
+
+			$('#countdown').text(`${minutes}:${seconds}`);
+			timeLeft--;
+		}
+	}
+
+	const timerInterval = setInterval(updateTimer, 1000);
 });
